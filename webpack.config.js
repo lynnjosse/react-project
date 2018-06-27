38c40a1196f791207ca37ada2d7a6215b47471e7
brthie1 <brthie1@monsanto.com>
2018-06-27 10:09:06 -0500

    Implement babel dependencies and set up Webpack with babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,16 @@ module.exports = {
 
     entry: `${path.join(__dirname, 'src')}/index.jsx`,
 
+    module: {
+        rules: [
+            {
+                loader:     'babel-loader',
+                test:       /\.js(x)?/,
+                include:    path.join(__direname, 'src')
+            }
+        ]
+    },
+
     output: {
         filename:   'bundle.js',
         path:       path.join(__dirname, 'public')
